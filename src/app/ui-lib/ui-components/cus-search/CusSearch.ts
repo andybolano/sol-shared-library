e5442cf179a93e1077466f8022e581002d00fd8f
Andy <andyyairb@gmail.com>
2024-01-28 00:26:19 -0500

    refactor CusSearch component

--- src/app/ui-lib/ui-components/cus-search/CusSearch.ts
+++ src/app/ui-lib/ui-components/cus-search/CusSearch.ts
@@ -1,14 +1,19 @@
-import { SetupContext, defineComponent, ref, watch } from "vue"
+import {
+	SetupContext,
+	computed,
+	defineComponent,
+	nextTick,
+	ref,
+	watch,
+} from "vue"
 import CusInput from "../cus-input/CusInput.vue"
-import SearchResults from "./components/SearchResults.vue"
 import type { ItemType } from "./interfaces/ItemType"
-import clickOutside from "../../../shared/directives/ClickOutSide"
+import clickOutside from "@/app/shared/directives/ClickOutSide"
 
 export default defineComponent({
 	name: "CusSearch",
 	components: {
 		CusInput,
-		SearchResults,
 	},
 	directives: {
 		clickOutside,
@@ -43,41 +48,39 @@ export default defineComponent({
 			default: false,
 		},
 		modelValue: {
-			type: [String, Number],
+			type: String,
+			required: true,
 		},
 	},
 	setup(props, { emit }: SetupContext) {
-		const value = ref(props.modelValue)
+		const value = computed({
+			get() {
+				return props.modelValue
+			},
+			set(value) {
+				emit("update:modelValue", value)
+			},
+		})
+
 		const showList = ref(false)
-		const handleItemSelected = (item: ItemType) => {
-			value.value = item.name
-			emit("itemSelected", item)
+
+		const handleActiveOption = (option: ItemType) => {
+			closeList()
+			emit("changeOption", option)
 		}
 
-		const handleChange = (event: any) => {
-			if (event.target) {
-				emit("update:modelValue", event.target.value)
-			}
-		}
-
-		const handleClickOutside = () => {
+		const closeList = () => {
 			showList.value = false
 		}
 
-		watch(
-			() => props.modelValue,
-			(newValue, oldValue) => {
-				value.value = newValue
-				showList.value = oldValue
-					? props.isLoadingResults || props.itemsResults.length > 0
-					: false
-			}
-		)
+		watch([() => props.isLoadingResults, () => props.itemsResults], () => {
+			showList.value =
+				props.isLoadingResults || props.itemsResults.length > 0
+		})
 
 		return {
-			handleClickOutside,
-			handleItemSelected,
-			handleChange,
+			closeList,
+			handleActiveOption,
 			value,
 			showList,
 		}
--- src/app/ui-lib/ui-components/cus-search/CusSearch.vue
+++ src/app/ui-lib/ui-components/cus-search/CusSearch.vue
@@ -1,21 +1,25 @@
 <template>
-	<div class="cus-search" v-click-outside="handleClickOutside">
+	<div class="cus-search" v-click-outside="closeList">
 		<CusInput
 			:id="id"
 			:label="label"
 			:placeholder="placeholder"
 			:icon="'icon-search'"
 			required
-			@input="handleChange"
 			v-model="value"
 		/>
-		<SearchResults
-			:showList="showList"
-			:textLoading="textLoading"
-			:itemsResults="itemsResults"
-			:isLoadingResults="isLoadingResults"
-			@itemSelected="handleItemSelected"
-		/>
+		<div v-if="showList" class="cus-search__results">
+			<h5 v-if="isLoadingResults" class="flashes">{{ textLoading }}</h5>
+			<ul v-else-if="itemsResults.length > 0">
+				<li
+					v-for="(option, index) in itemsResults"
+					:key="index"
+					@click.stop="handleActiveOption(option)"
+				>
+					<slot name="option" :option="option"></slot>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 <script lang="ts" src="./CusSearch.ts"></script>
